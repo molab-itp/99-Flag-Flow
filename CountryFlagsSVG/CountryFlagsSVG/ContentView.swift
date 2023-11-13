23f9d58c390f6484a13ce16d74e30b30ab9d9756
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2023-11-12 19:07:52 -0500

    ContentView

--- CountryFlagsSVG/CountryFlagsSVG/ContentView.swift
+++ CountryFlagsSVG/CountryFlagsSVG/ContentView.swift
@@ -9,8 +9,8 @@ import SwiftUI
 import SVGView
 
 let strRef = "https://upload.wikimedia.org/wikipedia/commons/0/0a/Flag_of_Jamaica.svg";
-let strRef1 = "https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg"
-let strRef2 = "https://upload.wikimedia.org/wikipedia/commons/9/99/Flag_of_Guyana.svg"
+let strRef1 = "https://upload.wikimedia.org/wikipedia/commons/9/99/Flag_of_Guyana.svg"
+let strRef2 = "https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg"
 
 struct ContentView: View {
     var body: some View {
@@ -18,7 +18,6 @@ struct ContentView: View {
             Rectangle()
                 .background(Color(white: 0.9))
                 .foregroundStyle(Color(white: 0.8))
-            
             VStack {
                 SVGViewAsync(strRef: strRef)
                 SVGViewAsync(strRef: strRef1)
@@ -35,13 +34,12 @@ struct SVGViewAsync: View {
     @State var data: Data?
     var body: some View {
         VStack {
-            if let data = data {
+            if let data {
                 SVGView(data: data)
                     .frame(width: 200, height: 100)
             }
         }
         .task {
-            // print("task data before", data ?? "-none-")
             data = await asyncDataFor(url: strRef)
             // print("task data after", data ?? "-none-")
         }
