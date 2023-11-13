55e1ab7b46247796497fd7d71aa4a698fff9bd93
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2023-11-12 19:01:05 -0500

    Bundle-decode countries.json

--- CountryFlagsSVG/CountryFlagsSVG/ContentView.swift
+++ CountryFlagsSVG/CountryFlagsSVG/ContentView.swift
@@ -8,18 +8,12 @@
 import SwiftUI
 import SVGView
 
-//let aname = "Flag_of_Jamaica";
-//let aname = "pikachu";
-let aname = "JAM";
 let strRef = "https://upload.wikimedia.org/wikipedia/commons/0/0a/Flag_of_Jamaica.svg";
-//let strRef = "https://jht1493.net/external_media/country-svg/JAM.svg";
+let strRef1 = "https://upload.wikimedia.org/wikipedia/en/a/a4/Flag_of_the_United_States.svg"
+let strRef2 = "https://upload.wikimedia.org/wikipedia/commons/9/99/Flag_of_Guyana.svg"
 
 struct ContentView: View {
-    
-//    @State var data: Data?
-    
     var body: some View {
-        
         ZStack {
             Rectangle()
                 .background(Color(white: 0.9))
@@ -27,6 +21,8 @@ struct ContentView: View {
             
             VStack {
                 SVGViewAsync(strRef: strRef)
+                SVGViewAsync(strRef: strRef1)
+                SVGViewAsync(strRef: strRef2)
                 Text("Hello, world!")
             }
             .padding()
@@ -37,18 +33,17 @@ struct ContentView: View {
 struct SVGViewAsync: View {
     var strRef: String
     @State var data: Data?
-    
     var body: some View {
         VStack {
             if let data = data {
                 SVGView(data: data)
-                    .frame(width: 100, height: 50)
+                    .frame(width: 200, height: 100)
             }
         }
         .task {
             // print("task data before", data ?? "-none-")
             data = await asyncDataFor(url: strRef)
-            print("task data after", data ?? "-none-")
+            // print("task data after", data ?? "-none-")
         }
     }
 }
