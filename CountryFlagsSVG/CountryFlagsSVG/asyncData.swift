8f7d79a6f3e5e628dac97687604adc0f098681b3
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2023-11-15 10:19:55 -0500

    timeIntervalSinceReferenceDate

--- CountryFlagsSVG/CountryFlagsSVG/asyncData.swift
+++ CountryFlagsSVG/CountryFlagsSVG/asyncData.swift
@@ -9,7 +9,9 @@ import Foundation
 
 // Load and return the source of a url as Data
 func asyncDataFor(url: String) async -> Data! {
-//    print("asyncDataFor url", url)
+    print("asyncDataFor url", url)
+    let startTime = Date.timeIntervalSinceReferenceDate
+    
     guard let url = URL(string: url) else {
         print("asyncDataFor url nil")
         return nil;
@@ -18,9 +20,17 @@ func asyncDataFor(url: String) async -> Data! {
         print("asyncDataFor data nil")
         return nil
     }
-//    print("asyncDataFor data.count", data.count)
+    // print("asyncDataFor data.count", data.count)
+    let duration = Date.timeIntervalSinceReferenceDate - startTime
+    print("asyncDataFor duration", duration, "url", url)
     return data;
 }
+
+// https://swdevnotes.com/swift/2020/timing_swift_functions/
+//    let startTime = Date()
+//    let endTime = Date()
+//    let timetaken = DateInterval(start: startTime, end: endTime)
+//    let duration = timetaken.duration
 
 // Based on:
 // - [07-ImageEditDemoJSON](https://github.com/molab-itp/07-ImageEditDemoJSON)
