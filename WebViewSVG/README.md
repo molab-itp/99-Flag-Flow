331b069df7434faedca33acf6f269e67c3c9dce0
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2023-11-15 22:36:10 -0500

    WebViewSVG

---
+++ WebViewSVG/README.md
@@ -0,0 +1,6 @@
+# WebViewDemo
+
+- use UIViewRepresentable to show WKWebView for web content
+
+- flag svg source
+    - https://github.com/linssen/country-flag-icons/blob/master/countries.json
---
+++ WebViewSVG/WebViewSVG/WebViewSVG_App.swift
@@ -0,0 +1,17 @@
+//
+//  WebKitDemoApp.swift
+//  WebKitDemo
+//
+//  Created by jht2 on 3/28/23.
+//
+
+import SwiftUI
+
+@main
+struct WebViewSVG_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
